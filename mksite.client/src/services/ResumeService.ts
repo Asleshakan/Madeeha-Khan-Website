448379bcd779acbf80d147b3fc287957ec3df1e0
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-11-06 12:06:28 +0530

    Update ResumeService.ts

--- mksite.client/src/services/ResumeService.ts
+++ mksite.client/src/services/ResumeService.ts
@@ -1,5 +1,4 @@
-const baseUrl = process.env.NODE_ENV === "production"
-  ? "https://kaserverapp-geb4fmazezhscyht.canadacentral-01.azurewebsites.net/" 
+
 export async function getRelevantExperience<T>(url: string): Promise<T> {
   const response = await fetch(`${baseUrl}${url}`);
   const body = await response.json();
