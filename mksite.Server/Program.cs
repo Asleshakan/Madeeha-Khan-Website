5619a43b8fbb5f57d2956e3413b98f827e4991ef
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-11-06 12:18:06 +0530

    Update Program.cs

--- mksite.Server/Program.cs
+++ mksite.Server/Program.cs
@@ -1,42 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowClient",
-        builder =>
-        {
-            builder.WithOrigins("https://ashy-forest-061a30b10.5.azurestaticapps.net/") // Use specific origins in production
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Serve default files and static files
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI(x =>
+if (app.Environment.IsDevelopment())
 {
-    x.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API V1");
-    x.RoutePrefix = app.Environment.IsDevelopment() ? "swagger" : string.Empty;
-});
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
-app.UseCors("AllowClient");
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-// Fallback for SPA routing
+
 app.MapFallbackToFile("/index.html");
 
 app.Run();
