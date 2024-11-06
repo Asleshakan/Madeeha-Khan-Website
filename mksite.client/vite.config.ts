b20241d2274511bb1feafb66cfea29a5f3d31096
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-11-06 08:57:00 +0530

    Update vite.config.ts

--- mksite.client/vite.config.ts
+++ mksite.client/vite.config.ts
@@ -1,14 +1,11 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import { env } from 'process';
-import basicSsl from '@vitejs/plugin-basic-ssl'
-
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 const isProd = process.env.NODE_ENV === 'production';
-const target = isProd ? 'https://kamksiteserver.azurewebsites.net/' : 
+const target = isProd ? 'https://kamksiteserver.azurewebsites.net' : 
     (env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7012');
 
@@ -23,14 +20,14 @@ export default defineConfig({
         }
     },
     server: {
-        proxy: {
+        proxy: isProd ? {} : {  // Only apply proxy in local development
             '^/experience': {
                 target,
-                secure: isProd // Set secure to true in production
+                secure: false
             },
             '^/about': {
                 target,
-                secure: isProd // Set secure to true in production
+                secure: false
             }
         },
         port: 5173
