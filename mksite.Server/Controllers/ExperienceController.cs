80efc99da93f04fb166fb22fb26c24debc07db75
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-11-06 10:51:29 +0530

    Update ExperienceController.cs

--- mksite.Server/Controllers/ExperienceController.cs
+++ mksite.Server/Controllers/ExperienceController.cs
@@ -5,7 +5,7 @@ using mksite.Server.Models;
 namespace mksite.Server.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class ExperienceController : ControllerBase
 {
     private ExperienceData Experience = new();
